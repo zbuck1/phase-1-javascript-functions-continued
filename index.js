51cf91c22ae395dfb04693b8cb437b287ec28dd7
Zoe <buckzoe5@gmail.com>
2022-01-17 14:32:24 -0500

    commit tree a3fc4a4917233c4e2ad8a6462cd888c7b6c4d245

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(adjective = '*') {
+    const inner = function(byDefault = "special") {
+        return `You are ${adjective}${byDefault}${adjective}!`;
+    }
+    return inner;
+}
